c14da6f42472dda2888cce04b68297c7f44d6de4
Nicolas Yarosz <nick.yarosz@gmail.com>
2020-07-11 01:16:49 -0700

    Add reward related records to dataGen script

--- server/src/dev-data/seeds/csvGenerator.js
+++ server/src/dev-data/seeds/csvGenerator.js
@@ -11,56 +11,78 @@ const cliProgress = require('cli-progress');
 // our stream that will be passed as the first argument for our generator function as 'writer'
 const writeProjects = fs.createWriteStream('projects.csv');
 // write our CSV headers
-writeProjects.write('id, title, creator, subtitle, category, subcategory, location, heroImage, heroVideo, launchDate, campaignDuration, budget, fundingGoal,\n', 'utf8');
+writeProjects.write('projectID, rewardID, title, creator, subtitle, category, subcategory, location, heroImage, heroVideo, launchDate, campaignDuration, budget, fundingGoal,\n', 'utf8');
 
-const bar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+const multibar = new cliProgress.MultiBar({ clearOnComplete: false, hideCursor: true }, cliProgress.Presets.rect);
 
 function writeTenMillionProjects(writer, encoding, callback) {
   let numOfRecords = 10000000
-   //Start CLI progress bar
-  bar.start(numOfRecords, 0);
-  let i = numOfRecords;
-  let id = 0;
+  //Start CLI progress bar
+  const pBar = multibar.create(numOfRecords, 0);
+  // const rBar = multibar.create(null, 0);
+  let i = numOfRecords; // 9999999
+  let projectID = 0; // 1
+  let rewardID = 0; // 1
   function write() {
     let ok = true;
-    do {
+    // outer loop for projects (primary record)
+    while (i > 0 && ok) {
       i -= 1;
-      id += 1;
-      // stage our faker data
-      const title = faker.commerce.productName();
-      const pledgeAmount = Math.floor(faker.finance.amount());
-      const description = faker.lorem.paragraph().substring(0, 200);
-      const deliveryMonth = faker.date.month();
-      const deliveryYear = faker.date.future().getFullYear();
-      const shippingType = faker.company.bsAdjective();
-      const rewardQuantity = Math.floor(Math.random() * (500 - 1 + 1)) + 1;
-      const timeLimit = faker.random.number();
-      const projectId = faker.random.number();
-      const rewardItems = Array.from({ length: random.int(1, 6) }, () =>
-        faker.commerce.product()
-      ).join(',')
-      // format each CSV line
-      const data = `${id}, ${title},
-      ${pledgeAmount},
-      ${description},
-      ${deliveryMonth},
-      ${deliveryYear},
-      ${shippingType},
-      ${rewardQuantity},
-      ${timeLimit},
-      ${projectId},
-      ${rewardItems}\n`;
+      projectID += 1;
+      pBar.increment();
+
+      // if projectID is between 1 -6 #1 reward
+      // if projectID is between 6 -9  #1-3 rewards
+      // if projectID is between 9 -10  # 6-7 rewards
+
+      // inner loop for project rewards (related records)
+      const randomNumRewards = random.int(3, 6);
+      // if projectID is > 6 mil randomNumRewards += 2
+      // if projectID is > 9 mil randomNumRewards += 2
+
+
       // this is the last time!
       if (i === 0) {
-        bar.increment(); // <- progress bar
         writer.write(data, encoding, callback);
       } else {
-      // See if we should continue, or wait.
-      // Don't pass the callback, because we're not done yet
-      bar.increment(); // <- progress bar
-      ok = writer.write(data, encoding);
+        for (let j = 0; j <= randomNumRewards; j++)  {
+          rewardID += 1;
+
+          // rewards bar
+          // rBar.setTotal(j, 0);
+
+          // stage our faker data
+          const title = faker.commerce.productName();
+          const pledgeAmount = Math.floor(faker.finance.amount());
+          const description = faker.lorem.paragraph().substring(0, 200);
+          const deliveryMonth = faker.date.month();
+          const deliveryYear = faker.date.future().getFullYear();
+          const shippingType = faker.company.bsAdjective();
+          const rewardQuantity = Math.floor(Math.random() * (500 - 1 + 1)) + 1;
+          const timeLimit = faker.random.number();
+          const projectId = faker.random.number();
+          const rewardItems = Array.from({ length: random.int(1, 6) }, () =>
+            faker.commerce.product()
+          ).join(',')
+          // format each CSV line
+          var data = `${projectID}, ${rewardID}, ${title},
+          ${pledgeAmount},
+          ${description},
+          ${deliveryMonth},
+          ${deliveryYear},
+          ${shippingType},
+          ${rewardQuantity},
+          ${timeLimit},
+          ${projectId},
+          ${rewardItems}\n`;
+          // See if we should continue, or wait.
+          // Don't pass the callback, because we're not done yet
+          // rBar.increment(); // <- progress bar
+          ok = writer.write(data, encoding);
+          pBar.updateETA();
+        }
       }
-    } while (i > 0 && ok);
+    }
     if (i > 0) {
       // had to stop early!
       // write some more once it drains
@@ -75,7 +97,7 @@ const startTime = new Date().getTime();
 writeTenMillionProjects(writeProjects, 'utf-8', () => {
   const endTime = new Date().getTime();
   const elapsed = (((endTime - startTime) / 1000) / 60).toFixed(3);
-  bar.stop();
-  console.log(`Finished writing all updates to CSV in ${elapsed} minutes`);
+  multibar.stop();
+  console.log(`Finished writing all projects and rewards to CSV in ${elapsed} minutes`);
   writeProjects.end();
 })
